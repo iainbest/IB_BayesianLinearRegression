aeeb0d0f176c8e89dcbdd551d6d794859dce4d43
Iain Best <iain.best@hotmail.com>
2021-11-04 17:18:44 +0000

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.23

---
+++ src/IB_BayesianLinearRegression.jl
@@ -0,0 +1,5 @@
+module IB_BayesianLinearRegression
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using IB_BayesianLinearRegression
+using Test
+
+@testset "IB_BayesianLinearRegression.jl" begin
+    # Write your tests here.
+end
