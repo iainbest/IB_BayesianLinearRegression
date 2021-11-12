a0bb5cfd34678add64518258854781e65357e8cf
Iain Best <iain.best@hotmail.com>
2021-11-12 13:12:38 +0000

    Fix of fix of fix

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,7 +6,7 @@ using Test
     # Write your tests here.
 
     # BayesianLinearRegression.jl tests
-    @test tr(posterior(ones(10,2),ones(10),1.0,1.0)[3]) == [11.0 10.0;10.0 11.0]
+    @test posterior(ones(10,2),ones(10),1.0,1.0)[3] == [11.0 10.0;10.0 11.0]
 
     # EvidenceApproximation.jl tests
 
